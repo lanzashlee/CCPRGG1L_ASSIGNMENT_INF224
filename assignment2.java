3c0c1e3b7349b040652d50caf1704115507fb22f
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2022-10-17 19:06:55 +0800

    Create assignment2.java

---
+++ assignment2.java
@@ -0,0 +1,51 @@
+public class assignment2 {
+    public static void main(String[] args) throws Exception {
+        int count = 0;
+
+         while (count !=3){
+            System.out.println("Lanz");
+            count ++;
+        }
+        // do while loop
+
+         int i = 0;
+         do {
+            System.out.println("Dinglasa");
+             i ++;
+        } while (i != 4);
+
+        //for loop
+
+        for (int l = 0; l != 5;  l++) {
+            System.out.println("Ricamara");
+        }
+        // char array
+        
+        String[] myFirstName = {"L","A","N","Z","A","S","H","L","E","E"};
+
+        System.out.println(myFirstName[0]);
+        System.out.println(myFirstName[1]);
+        System.out.println(myFirstName[2]);
+        System.out.println(myFirstName[3]);
+        System.out.println(myFirstName[4]);
+        System.out.println(myFirstName[5]);
+        System.out.println(myFirstName[6]);
+        System.out.println(myFirstName[7]);
+        System.out.println(myFirstName[8]);
+        System.out.println(myFirstName[9]);
+
+        // char array in reverse
+        
+        String[] myfirstname = {"L","A","N","Z","A","S","H","L","E","E"};
+
+        System.out.println(myfirstname[9]);
+        System.out.println(myfirstname[8]);
+        System.out.println(myfirstname[7]);
+        System.out.println(myfirstname[6]);
+        System.out.println(myfirstname[5]);
+        System.out.println(myfirstname[4]);
+        System.out.println(myfirstname[2]);
+        System.out.println(myfirstname[1]);
+        System.out.println(myfirstname[0]);
+}
+}
